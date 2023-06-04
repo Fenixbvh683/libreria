04d661a1e65d8e5f8ef1b8aa067d98a8b3f6fdf4
Aldoleonel <aldoleonelchalup@gmail.com>
2023-06-04 18:41:49 -0300

    agregue funciones listar y editar

---
+++ productos/editar.js
@@ -0,0 +1,25 @@
+const { leerJSON, escribirJSON } = require("../data");
+const productos_db = leerJSON();
+const editar = function(id){
+
+    const productoAModificar = productos_db.find(producto => producto.id === id);
+
+    if(!productoAModificar){
+      return `Mmmmm, 404 NOT FOUND`
+    }
+
+    const {nombre, marca} = productoAModificar;
+    const productosModificados =  this.productos.map(producto => {
+      
+      if(producto.id === id){
+        producto.stock = !producto.stock
+      }
+      
+      return producto
+    })
+
+    escribirJSON(productosModificados);
+
+    return `El producto "${nombre} | ${marca}" se modificó satisfactoriamente`
+  }
+  module.exports = editar;
